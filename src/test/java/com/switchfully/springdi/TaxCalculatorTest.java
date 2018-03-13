18d6fd1073a51e5b39ea611efd219f7a3a20e5c7
MRedant <mredant@gmail.com>
2018-03-13 15:00:34 +0100

    tests included (with and without mockito)

--- src/test/java/com/switchfully/springdi/TaxCalculatorTest.java
+++ src/test/java/com/switchfully/springdi/TaxCalculatorTest.java
@@ -1,23 +1,49 @@
 package com.switchfully.springdi;
 
 import com.switchfully.springdi.taxByCountry.AmericanTaxCalc;
+import com.switchfully.springdi.taxByCountry.TaxCalculation;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.mockito.internal.verification.Times;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+import static org.mockito.ArgumentMatchers.any;
+
 public class TaxCalculatorTest {
 
     @Test
-    public void givenYearSalaryAndCountry_calculateTaxesOnYearIncome() {
+    public void integrationTest_calculateTaxesOnYearIncome_givenYearSalaryAndCountry_calculateTaxesOnYearIncome() {
         //GIVEN
         TaxCalculator calculator = new TaxCalculator(new AmericanTaxCalc());
+        BigDecimal expectedResult = BigDecimal.valueOf(8150);
+
         //WHEN
         BigDecimal yearSalary = BigDecimal.valueOf(40000);
         BigDecimal actualResult = calculator.calculateTaxesOnYearIncome(yearSalary).setScale(0, RoundingMode.CEILING);
-        BigDecimal expectedResult = BigDecimal.valueOf(8150);
+
         //THEN
         Assertions.assertEquals(actualResult,expectedResult);
     }
+    @Test
+    public void unitTest_calculateTaxesOnYearIncome_givenYearSalaryAndCountry_calculateTaxesOnYearIncome() {
+        //GIVEN
+        TaxCalculation taxCalculation = Mockito.mock(AmericanTaxCalc.class);
+        TaxCalculator calculator = new TaxCalculator(taxCalculation);
+        BigDecimal yearSalary = BigDecimal.valueOf(40000);
+        BigDecimal expectedResult = BigDecimal.valueOf(8150);
+
+        //WHEN
+        Mockito.when(taxCalculation.calculateTax(any())).thenReturn(BigDecimal.valueOf(8150));
+        BigDecimal actualResult = calculator.calculateTaxesOnYearIncome(yearSalary);
+
+        //THEN
+        Mockito.verify(taxCalculation,new Times(1)).calculateTax(any());
+        Assertions.assertEquals(actualResult,expectedResult);
+    }
+
+
+
 }
